8c2c2ec812d9cb5ae950aee3fa7046b1d9f7cd54
abdillah-zikri <137777289+abdillah-zikri@users.noreply.github.com>
2023-10-16 08:32:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/app', // Set your subdirectory here
-  assetPrefix: '/app/', // Set your subdirectory here
+//   output: 'export',
+//   basePath: '/app', // Set your subdirectory here
+//   assetPrefix: '/app/', // Set your subdirectory here
 }
 
 module.exports = nextConfig
